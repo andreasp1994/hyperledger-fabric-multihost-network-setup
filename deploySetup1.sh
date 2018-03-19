99a8a2816393d7a97af534e4c5511ffec1e3b507
Andreas Poyiatzis <Andreas@apogiatzis.com>
2018-03-19 14:56:13 +0000

    added another peer on PC1

--- deploySetup1.sh
+++ deploySetup1.sh
@@ -18,3 +18,4 @@ docker exec peer0.org1.example.com peer channel create -o orderer.example.com:70
 
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
+docker exec -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" peer1.org1.example.com peer channel join -b mychannel.block
