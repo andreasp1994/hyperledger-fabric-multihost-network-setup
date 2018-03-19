4089b95c118a080659333cc1b532f82fcaabbf4f
Andreas Poyiatzis <Andreas@apogiatzis.com>
2018-03-19 15:46:57 +0000

    added more sleep

--- deploySetup1.sh
+++ deploySetup1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DELAY=5
+DELAY=10
 
 export ARCH=`uname -m`
 
@@ -18,4 +18,4 @@ docker exec peer0.org1.example.com peer channel create -o orderer.example.com:70
 
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
-docker exec -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" peer1.org1.example.com peer channel join -b mychannel.block
+docker exec -e "CORE_PEER_ADDRESS=peer1.org1.example.com:7051" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
