99dc05316cac2a950f3535661bfebdce923d49fd
Andreas Poyiatzis <Andreas@apogiatzis.com>
2018-03-20 03:32:55 +0000

    down to 1

--- composer_deploy.sh
+++ composer_deploy.sh
@@ -3,7 +3,7 @@ cp crypto_material/crypto-config/peerOrganizations/org1.example.com/users/Admin\
 
 cp crypto_material/crypto-config/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp/keystore/d23867d612a7070992ff485b0534b7a269ca8b7b77c5e73a3f9af3eb49d88fbd_sk admin_secret_key
 
-composer card create -p connection_setup2.json -u PeerAdmin -c Admin@org1.example.com-cert.pem -k admin_secret_key -r PeerAdmin -r ChannelAdmin
+composer card create -p connection_setup1.json -u PeerAdmin -c Admin@org1.example.com-cert.pem -k admin_secret_key -r PeerAdmin -r ChannelAdmin
 
 composer card import -f PeerAdmin@setup1.card
 
